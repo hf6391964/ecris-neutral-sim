39e73e4d20a7e20042c60302da0827ef3ebbef2a
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2016-10-19 22:47:40 +0300

    Implement Lotz equation

--- element_data.h
+++ element_data.h
@@ -1,12 +1,13 @@
 #ifndef ELEMENT_DATA_H
 #define ELEMENT_DATA_H
 
-#define MAX_Z 60
+#define MAX_SHELLS 20
 
 struct ElementData {
     unsigned int Z;  // proton number
     double mass;  // in atomic mass units
-    double ionizationEnergies[MAX_Z];
+    double ionizationEnergies[MAX_SHELLS];
+    unsigned int electronPopulation[MAX_SHELLS];
 };
 
 #endif
--- lotz.h
+++ lotz.h
@@ -1,10 +1,17 @@
 #ifndef LOTZ_H
 #define LOTZ_H
 
+#include "element_data.h"
+
 struct LotzParameters {
-    double ai[MAX_Z];
-    double bi[MAX_Z];
+    double ai[MAX_SHELLS];
+    double bi[MAX_SHELLS];
+    double ci[MAX_SHELLS];
 };
+
+double ionizationCrossSection(unsigned int N, double electronEnergy,
+    LotzParameters parameters, double ionizationEnergies[],
+    unsigned int electronPopulation[]);
 
 #endif
 
