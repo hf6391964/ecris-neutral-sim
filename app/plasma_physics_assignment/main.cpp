5f6dd70545b5f1af2c1057aa92aa2d07923bee84
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2016-10-24 12:07:56 +0300

    Add hexapole B plot

--- app/plasma_physics_assignment/main.cpp
+++ app/plasma_physics_assignment/main.cpp
@@ -14,13 +14,27 @@ int main() {
     // B0, r0, dt, z1, z2
     ElectronModel model(B0, r0, dt, z1, z2);
 
-    // Plot Bz(r=0, z)
+    // Plot solenoid Bz(r=0, z)
     int nPoints = 100;
     std::ofstream fout("output/solenoid_B_onaxis.csv");
     for (int i = 0; i <= nPoints; i++) {
         double z = z1 + (z2 - z1) * (double)i/nPoints;
         Vector B = ElectronModel::solenoidBfield(0.0, 0.0, z);
         fout << z << CSV_SEP << B.z() << std::endl;
+    }
+    fout.close();
+
+    // Plot hexapole Bxy(x, y)
+    fout.open("output/hexapole_B.csv");
+    nPoints = 15;
+    for (int i = 0; i <= nPoints; i++) {
+        double y = -r0 + 2.0*r0 * (double)i/nPoints;
+        for (int j = 0; j <= nPoints; j++) {
+            double x = -r0 + 2.0*r0 * (double)j/nPoints;
+            Vector B = ElectronModel::hexapoleBfield(x, y, B0, r0);
+            fout << x << CSV_SEP << y << CSV_SEP << B.x() << CSV_SEP <<
+                B.y() << std::endl;
+        }
     }
     fout.close();
 
--- app/plasma_physics_assignment/output/makeplots.py
+++ app/plasma_physics_assignment/output/makeplots.py
@@ -2,10 +2,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+# Solenoid B field
 data = np.loadtxt('solenoid_B_onaxis.csv', delimiter=';')
-
+plt.figure()
 plt.plot(data[:, 0], data[:, 1])
 plt.xlabel('z (m)')
 plt.ylabel('B (T)')
 plt.xlim([-0.16, 0.17])
 plt.savefig('solenoid_B_onaxis.png')
+
+# Hexapole B field
+data = np.loadtxt('hexapole_B.csv', delimiter=';')
+plt.figure()
+plt.quiver(data[:, 0], data[:, 1], data[:, 2], data[:, 3])
+plt.gca().set_aspect('equal')
+plt.xlabel('x (m)')
+plt.ylabel('y (m)')
+plt.savefig('hexapole_B.png')
+
+plt.show()
