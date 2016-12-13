15524a36d1397ef73d5ccd9a7e055bf4aa5bda5d
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2016-12-13 17:20:00 +0200

    Decrease resolution of reaction rates

--- lib/simpleplasmamodel.cpp
+++ lib/simpleplasmamodel.cpp
@@ -55,7 +55,7 @@ void SimplePlasmaModel::populateCollisionReactions(
 
     double mbave = Util::getMBAverage(maxIonTemp_, ionMassEv_);
 
-    generator.precomputeReactionRates(8.0*mbave, mbave/1000.0, thread_res);
+    generator.precomputeReactionRates(8.0*mbave, mbave/100.0, thread_res);
 }
 
 void SimplePlasmaModel::setCoordinateTransformation(
