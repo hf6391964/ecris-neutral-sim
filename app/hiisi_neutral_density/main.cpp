c07358498be8c4ec0cd6ce0b312d229c0fd61e72
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2016-12-14 17:25:56 +0200

    Update simulation parameters

--- app/hiisi_neutral_density/main.cpp
+++ app/hiisi_neutral_density/main.cpp
@@ -5,20 +5,21 @@
 #include "argon_data.h"
 
 int main() {
-    const double ELECTRON_DENSITY = 1e17;
+    const double ELECTRON_DENSITY = 1e22;
     const double ELECTRON_TEMPERATURE = 10e3;
     const ElementData ELEM_DATA = ARGON_DATA;
     const double ION_TEMPERATURE = 4.0;
     std::vector<double> ION_RELATIVE_DENSITIES = {
         1.0/2.0, 1.0/4.0, 1.0/8.0, 1.0/16.0, 1.0/32.0, 1.0/64.0, 1.0/64.0
     };
-    const double GRID_SIZE = 0.005;
+    const double GRID_SIZE = 0.01;
+    const size_t N_PARTICLES = 10000000;
 
     std::vector<double> ION_TEMPERATURES(ION_RELATIVE_DENSITIES.size(),
         ION_TEMPERATURE);
     SimplePlasmaModel plasmamodel(
         "electron_density_hiisi.csv",
-        ELECTRON_WEIGHT, ION_RELATIVE_DENSITIES, ELECTRON_TEMPERATURE,
+        ELECTRON_DENSITY, ION_RELATIVE_DENSITIES, ELECTRON_TEMPERATURE,
         ION_TEMPERATURES, ELEM_DATA);
 
     simthreadresources *thread_res = Util::allocateThreadResources(13122016);
@@ -34,13 +35,13 @@ int main() {
     simModel.addSurface(&radial_wall);
     simModel.addSurface(&end1);
     simModel.addSurface(&end2);
-    SurfaceEmission gasFeed(&end1, 1e-12);
+    SurfaceEmission gasFeed(&radial_wall, 1e-12);
     simModel.addSource(&gasFeed);
 
     CollisionGenerator generator(simModel.getGrid(GRID_SIZE));
     plasmamodel.populateCollisionReactions(generator, thread_res);
 
-    simModel.runSimulation(generator, 1000, "test", true, 0.01, 0.1, 2.0, 4);
+    simModel.runSimulation(generator, N_PARTICLES, "test", true, GRID_SIZE, 0.1, 2.0, 4);
 
     Util::deallocateThreadResources(thread_res);
 
