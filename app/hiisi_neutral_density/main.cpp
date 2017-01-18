17008fad2147476a121036c5bba18ed6f27759ba
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-01-18 10:55:04 +0200

    Update hiisi_neutral_density to new APIs

--- app/hiisi_neutral_density/main.cpp
+++ app/hiisi_neutral_density/main.cpp
@@ -10,7 +10,8 @@ int main() {
     const Element ELEMENT = ARGON;
     const double ION_TEMPERATURE = 4.0;
     std::vector<double> ION_RELATIVE_DENSITIES = {
-        1.0/4096.0, 1.0/1024.0, 1.0/256.0, 1.0/64.0, 1.0/16.0, 1.0/8.0, 1.0/2.0, 1.0/8.0, 1.0/16.0, 1.0/64.0, 1.0/256.0, 1.0/1024.0, 1.0/4096.0
+        1.0/4096.0, 1.0/1024.0, 1.0/256.0, 1.0/64.0, 1.0/16.0, 1.0/8.0, 1.0/2.0,
+        1.0/8.0, 1.0/16.0, 1.0/64.0, 1.0/256.0, 1.0/1024.0, 1.0/4096.0
     };
     const double GRID_SIZE = 0.005;
     const size_t N_PARTICLES = 100000000;
@@ -23,7 +24,6 @@ int main() {
         ION_TEMPERATURES, ELEMENT);
 
     simthreadresources *thread_res = Util::allocateThreadResources(13122016);
-    SimulationModel simModel;
 
     Surface radial_wall("model/radial_wall.stl",
         0.0, ROOM_TEMPERATURE_EV, false, 0.1);
@@ -31,11 +31,13 @@ int main() {
         1.0, ROOM_TEMPERATURE_EV, true, 0.1);
     Surface end2("model/end2.stl",
         1.0, ROOM_TEMPERATURE_EV, false, 0.1);
-
-    simModel.addSurface(&radial_wall);
-    simModel.addSurface(&end1);
-    simModel.addSurface(&end2);
+    SurfaceCollection surfaces;
+    surfaces.addSurface(&radial_wall);
+    surfaces.addSurface(&end1);
+    surfaces.addSurface(&end2);
     SurfaceEmission gasFeed(&radial_wall, 1e-12, ELEMENT);
+
+    SimulationModel simModel(surfaces);
     simModel.addSource(&gasFeed);
 
     CollisionGenerator generator(simModel.getGrid(GRID_SIZE));
