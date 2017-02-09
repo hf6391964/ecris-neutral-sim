e2bad52dd10c9b05a4b8444271fea7e205af01f8
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-02-09 21:18:25 +0200

    New parameters for electron simulation

--- app/hiisi_electron_density/main.cpp
+++ app/hiisi_electron_density/main.cpp
@@ -8,19 +8,19 @@ int main() {
                  z2 = 196.2e-3,
                  r0 = 55e-3,
                  B0 = 1.188,
-                 dt = 5e-12,
+                 dt = 2e-12,
                  confinementTime = 1e-6,
-                 gridSize = 0.005,
-                 Becr = 0.643;
+                 gridSize = 0.001,
+                 Becr = 0.656;
     const double Ai[] =
         { 0.413286, 0.954437, 52.1123, -74.7674, -1258.17, 2994.75, 22469.9 };
-    const unsigned long N_PARTICLES = 10000;
+    const unsigned long N_PARTICLES = 1000000;
 
     // B0, r0, dt, z1, z2
     ElectronModel model(B0, r0, dt, z1, z2, gridSize, confinementTime, Ai);
 
     Rng rng(2016);
-    model.runMonoenergeticSimulation(N_PARTICLES, 10e3, Becr, rng);
+    model.runMaxwellSimulation(N_PARTICLES, 10e3, Becr, rng);
 
     model.writeDensityToFile("electron_density_hiisi.csv");
     model.writeElectronEndpoints("z1_collision_points.csv",
