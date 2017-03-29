a220f3284062d1639d1a95db7ff8bae5c69ebddc
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-03-29 17:09:38 +0300

    Comments in the main simulation file

--- app/hiisi_neutral_density/main.cpp
+++ app/hiisi_neutral_density/main.cpp
@@ -12,30 +12,45 @@ void doRun(size_t N_PARTICLES, double PULSE_LENGTH = 1.0e-3,
     double SAMPLING_INTERVAL = 0.005, size_t N_TIME_SAMPLES = 0,
     uint_least32_t seed = 13122016) {
 
+    // Set this to true to get detailed data about particle trajectories.
+    // Useful for debugging with small particle numbers (< 100).
     const bool PARTICLE_LOOP_LOGGING = false;
-    const double ELECTRON_DENSITY = 1e17;
-    const double ELECTRON_TEMPERATURE = 10e3;
+
+    const double ELECTRON_DENSITY = 1e17;  // 1 / m^3
+    const double ELECTRON_TEMPERATURE = 10e3;  // eV
     const Element ELEMENT = ARGON;
-    const double ION_TEMPERATURE = 1.0;
+    const double ION_TEMPERATURE = 1.0;  // eV
     std::vector<double> ION_RELATIVE_DENSITIES {
         0.0729343, 0.04383817, 0.03905347, 0.03666112, 0.0586578, 0.05133851,
         0.06074164, 0.13743072, 0.1514723, 0.13927348, 0.10523982, 0.06349422,
         0.02817102, 0.00892281, 0.00243114, 0.00033945
     };
-    const double GRID_SIZE = 0.001;
-    const double ESCAPE_TIME = 1.9e-3;
-    const double EXTRACTION_EFFICIENCY = 0.1;
-    const double CUTOFF_TIME = 5.0;
+    const double GRID_SIZE = 0.001;  // metre
+    const double CONFINEMENT_TIME = 1.9e-3;  // seconds
+    const double EXTRACTION_EFFICIENCY = 0.1;  // Extraction efficiency factor
+    const double CUTOFF_TIME = 5.0;  // A time at which the tracking of an individual particle is terminated
 
+    // Create a vector of ion temperatures, the same value for every population
+    // by default
     std::vector<double> ION_TEMPERATURES(ION_RELATIVE_DENSITIES.size(),
         ION_TEMPERATURE);
+    // Create the plasma model, which is responsible for populating the
+    // collision reaction data with the right parameters
     SimplePlasmaModel plasmamodel(
         "electron_data/electron_density_hiisi.csv",
         ELECTRON_DENSITY, ION_RELATIVE_DENSITIES, ELECTRON_TEMPERATURE,
         ION_TEMPERATURES, ELEMENT);
 
-    SurfacePtr chamber = std::make_shared<Surface>("real-model/chamber.stl",
-        0.0, ROOM_TEMPERATURE_EV, "chamber wall", false, 0.001);
+    // Surfaces are shared between various objects, so they are allocated by
+    // make_shared
+    SurfacePtr chamber = std::make_shared<Surface>(
+        "real-model/chamber.stl",  // path to the STL
+        0.0,  // pumping probability
+        ROOM_TEMPERATURE_EV,  // surface temperature
+        "chamber wall",  // surface label
+        false,  // flip normals?
+        0.001  // scaling factor. use 0.001 for STLs that are in millimeters
+    );
     SurfacePtr surrounding_cylinder = std::make_shared<Surface>(
         "real-model/sheath.stl",
         1.0, ROOM_TEMPERATURE_EV, "surrounding cylinder", true, 0.001);
@@ -46,31 +61,56 @@ void doRun(size_t N_PARTICLES, double PULSE_LENGTH = 1.0e-3,
         "real-model/extraction_surface.stl",
         EXTRACTION_EFFICIENCY, ROOM_TEMPERATURE_EV, "extraction surface", false,
         0.001);
+    // The whole simulation geometry is defined as a surface collection. Add
+    // all the loaded surfaces to it.
     SurfaceCollection surfaces;
     surfaces.addSurface(chamber);
     surfaces.addSurface(surrounding_cylinder);
     surfaces.addSurface(injection_surface);
     surfaces.addSurface(extraction_surface);
+    // Create the simulation model object which is responsible for the control
+    // flow.
     SimulationModel simModel(surfaces);
+    // Add a particle source. Multiple sources can be defined, resulting in
+    // different output files tagged with the label to be generated.
     simModel.addSource(std::make_unique<SurfaceEmission>(
-        injection_surface, PARTICLES_PER_SECOND, ELEMENT, "injection"));
+        injection_surface,  // the surface object
+        PARTICLES_PER_SECOND,  // particle emission rate
+        ELEMENT,  // the element of the particles
+        "injection"  // the label of the neutral source
+    ));
 
+    // Initialize a collision generator and populate the collision reactions
     CollisionGenerator generator(simModel.getGrid(GRID_SIZE));
+    plasmamodel.populateCollisionReactions(generator,
+        14032017,  // seed for Monte Carlo integration of the rate coefficients
+        0.01  // Resolution of the rate coefficient LUT. Fraction of the maximal ion population temperature
+    );
+    // Initialize a neutralization generator and populate the neutralization
+    // reactions
     NeutralizationGenerator ngenerator;
-    plasmamodel.populateNeutralizationReactions(ngenerator, ESCAPE_TIME,
+    plasmamodel.populateNeutralizationReactions(ngenerator,
+        CONFINEMENT_TIME,
         "electron_data/cylinder_collision_points.csv",
         "electron_data/z1_collision_points.csv",
         "electron_data/z2_collision_points.csv",
         "rt.018.dat", surfaces, ELECTRON_DENSITY);
-    plasmamodel.populateCollisionReactions(generator, 14032017, 0.01);
 
     std::string name = "test_" + std::to_string(N_PARTICLES);
     logger.setLogging(PARTICLE_LOOP_LOGGING);
     clock_t start_clock = clock();
     time_t start_time = time(NULL);
     simModel.runSimulation(generator, ngenerator,
-        N_PARTICLES, name, N_TIME_SAMPLES, GRID_SIZE, SAMPLING_INTERVAL,
-        PULSE_LENGTH, CUTOFF_TIME, -1, seed);
+        N_PARTICLES,  // number of test particles
+        name,  // the file name prefix
+        N_TIME_SAMPLES,  // number of sampled frames in the case of time-dependent simulation. 0 for stationary
+        GRID_SIZE,  // grid size (metres)
+        SAMPLING_INTERVAL,  // sampling interval (seconds)
+        PULSE_LENGTH,  // the length of the injected gas pulse
+        CUTOFF_TIME,
+        -1,  // number of threads. -1 for automatic: 1 + 2 * hardware concurrency
+        seed  // random seed
+    );
     clock_t end_clock = clock();
     time_t end_time = time(NULL);
 
@@ -106,7 +146,8 @@ void run_time_dependent() {
 }
 
 int main() {
-    run_time_dependent();
+    run_stationary(100000);
+    //run_time_dependent();
 
     return EXIT_SUCCESS;
 }
